48a8c27f1e4bb4abaaaac1b9049d6891c0d07264
Vinay Pandey <engg.vinaypandey@gmail.com>
2022-02-02 16:56:02 +0530

    Moved all the HTML documentations in separate Github Pages

--- Documents/java_doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.qa.bank"},{"l":"com.qa.bank.configuration"},{"l":"com.qa.bank.controller"},{"l":"com.qa.bank.data.entity"},{"l":"com.qa.bank.data.repository"},{"l":"com.qa.bank.exceptions"},{"l":"com.qa.bank.service"}];updateSearchResults();
